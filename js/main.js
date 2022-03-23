d6b394a9b47b077c20a16b877ffa52bb7f029dbe
CamONE1 <kammnsn@me.com>
2022-03-23 23:35:46 +0300

    changed information

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const navBtn = document.querySelector('.nav-button');
 const mblNav = document.querySelector('.mobile-nav');
+const body = document.body;
 
 navBtn.addEventListener('click', function () {
     mblNav.classList.toggle('mobile-nav-active');
     navBtn.classList.toggle('nav-button-close');
+    body.classList.toggle('no-scroll');
 })
